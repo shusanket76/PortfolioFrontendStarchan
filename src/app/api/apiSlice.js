a6528e07d25f5073b995ead3c34e417443eed4a0
Shusanket <shusanketbasyal76@gmail.com>
2023-07-03 22:27:23 -0400

    BaseUrl

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3500" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://portfolio-470r.onrender.com",
+  }),
   tagTypes: ["Modeling", "Nature", "Poetry", "ClothDesign", "Clothes"],
   endpoints: (builder) => ({}),
 });
